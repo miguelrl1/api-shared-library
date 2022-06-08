d654a14ef3bae4c010584188fdb457bef17061f7
miguelrl1 <miguel.rochalona@gmail.com>
2022-06-07 20:26:52 -0500

    add transitionId
    add transitionId

--- vars/jiraTransitionIssue.groovy
+++ vars/jiraTransitionIssue.groovy
@@ -2,7 +2,7 @@ def call(Map config=[:]) {
   def rawBody = libraryResource 'com/devopsbox/api/jira/transitionIssue.json'
   def binding = [
     key: "${config.key}",
-    transitionId: "${config.id}"
+    idTransition: "${config.id}"
   ]
   def render = renderTemplate(rawBody,binding)
   sh('curl -x $URL_PROXY -D- -u $JIRA_CREDENTIALS -X POST --data "'+render+'" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue/$key/transitions')
