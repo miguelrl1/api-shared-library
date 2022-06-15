77a9a13dead38091129466246025a6f2c87f1194
miguelrl1 <miguel.rochalona@gmail.com>
2022-06-14 19:52:15 -0500

    Update jiraAddComment.groovy

--- vars/jiraAddComment.groovy
+++ vars/jiraAddComment.groovy
@@ -1,17 +1,12 @@
 def call(Map config=[:]) {
-  def issueId = getIssueIdFromCommitMessage()
-  //echo "issueId = ${issueId}"
-  if( !issueId?.trim() ) {
-    //echo "bailing out"
-    return
-  }
+  
   def rawBody = libraryResource 'com/devopsbox/api/jira/addComment.json'
   def binding = [
+    key: "${config.key}",
     body: "${config.body}"
   ]
   def render = renderTemplate(rawBody,binding)
-  def jiraServer = "https://jiraserver"
-  sh """
-    curl -x $URL_PROXY -D- -u $JIRA_CREDENTIALS -X POST --data \"${render}\" -H \"Content-Type: application/json\" $jiraServer/rest/api/2/issue/${issueId}/comment
-  """
+  def issueId = "${config.key}"
+
+  sh('curl -x $URL_PROXY -D- -u $JIRA_CREDENTIALS -X POST --data "'+render+'" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue/"'+issueId+'"/comment')
 }
